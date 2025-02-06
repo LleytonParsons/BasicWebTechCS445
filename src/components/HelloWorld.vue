364198f0d92859a2ea5ed03bc660dfa458cfebcf
LleytonParsons <168110045+LleytonParsons@users.noreply.github.com>
2025-02-06 18:00:29 -0500

    Little change to counter system.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,28 +5,28 @@ defineProps({
   msg: String,
 })
 
-const count = ref(0)
+const count1 = ref(0)
+const count2 = ref(0)
+const count3 = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <body>
-      <h2>Eggs</h2>
-      <button type="button" @click="count--">-</button>
-      <h2>{{ count }}</h2>
-      <button type="button" @click="count++">+</button>
-      <h2>Milk</h2>
-      <button type="button" @click="count--">-</button>
-      <h2>{{ count }}</h2>
-      <button type="button" @click="count++">+</button>
-      <h2>Lettuce</h2>
-      <button type="button" @click="count--">-</button>
-      <h2>{{ count }}</h2>
-      <button type="button" @click="count++">+</button>
-    </body>
-  </div>
+  <body>
+    <h2>Eggs</h2>
+    <button type="button" @click="count1--">-</button>
+    <h2>{{ count1 }}</h2>
+    <button type="button" @click="count1++">+</button>
+    <h2>Milk</h2>
+    <button type="button" @click="count2--">-</button>
+    <h2>{{ count2 }}</h2>
+    <button type="button" @click="count2++">+</button>
+    <h2>Lettuce</h2>
+    <button type="button" @click="count3--">-</button>
+    <h2>{{ count3 }}</h2>
+    <button type="button" @click="count3++">+</button>
+  </body>
 </template>
 
 <style scoped>
