a7a03053426c3cb77010f5ba242c477a132c8bc3
LleytonParsons <168110045+LleytonParsons@users.noreply.github.com>
2025-02-06 17:26:49 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <body>
+      <h2>Eggs</h2>
+      <button type="button" @click="count--">-</button>
+      <h2>{{ count }}</h2>
+      <button type="button" @click="count++">+</button>
+      <h2>Milk</h2>
+      <button type="button" @click="count--">-</button>
+      <h2>{{ count }}</h2>
+      <button type="button" @click="count++">+</button>
+      <h2>Lettuce</h2>
+      <button type="button" @click="count--">-</button>
+      <h2>{{ count }}</h2>
+      <button type="button" @click="count++">+</button>
+    </body>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
